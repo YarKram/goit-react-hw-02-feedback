43ebbc00b4c08fede58c055944e36ff36f95ec4d
Yaroslav <yaroslav.kramarchuk@gmail.com>
2023-02-01 15:41:03 +0100

    first-task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,66 @@
-export const App = () => {
-  return (
-    <>
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-        }}
-      >
-        React homework template
-      </div>
-    </>
-  );
-};
+import { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notification/Notification';
+import Section from './Section/Section';
+
+import { AppStyled } from './App.styled';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateState = e => {
+    const { name } = e.currentTarget;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, bad, neutral } = this.state;
+    const total = good + bad + neutral;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    let expression = 0;
+    expression = Math.round((100 * good) / this.countTotalFeedback());
+    return expression + '%';
+  };
+
+  render() {
+    const { good, bad, neutral } = this.state;
+    const total = good + bad + neutral;
+    return (
+      <AppStyled>
+        <Section title="Please leave a feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.updateState}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback 😔" />
+          )}
+        </Section>
+      </AppStyled>
+    );
+  }
+}
+
+export default App;
